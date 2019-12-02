8838d0524884f77c17de4885304f2f646d2bf765
Deph0 <Deph0@users.noreply.github.com>
2019-12-02 14:09:09 +0100

    Solved day 2 pt 2

--- 2019/intcode.py
+++ 2019/intcode.py
@@ -2,7 +2,12 @@
 # https://adventofcode.com/2019/day/2
 # start 7:19am 
 # started over at 7:37am confused af
-# solved 8:19am
+# solved pt 1 8:19am
+#
+# AdventOfCode 2019 day 2 pt 2
+# start pt2 8:55 - paused 9:30
+# unpaused 13:30
+# solved pt2 13:50
 
 def calcPt1(intcode):
     i = 0
@@ -29,9 +34,22 @@ def calcPt1(intcode):
 
 filepath = 'input_2019-2.txt'
 with open(filepath) as fp:
-    intcode = [int(x) for x in fp.readline().split(",")]
-    # print(intcode)
+    originalintcode = [int(x) for x in fp.readline().split(",")]
+    intcode = [x for x in originalintcode]
     intcode[1] = 12
     intcode[2] = 2
     result = calcPt1(intcode)
-    print(result[0])
+    print(result[0]) # 3716250
+
+    # pt 2
+    for noun in range(100):
+        for verb in range(100):
+            xintcode = [x for x in originalintcode]
+            xintcode[1] = noun
+            xintcode[2] = verb
+            
+            result = calcPt1(xintcode)
+        
+            if result[0] == 19690720:
+                print( (100*noun)+verb ) # 6472
+                break
