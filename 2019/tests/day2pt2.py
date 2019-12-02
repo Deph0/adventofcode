f6c2d8a9a17e2bea07177237ca62dc04dec7b3d5
Deph0 <Deph0@users.noreply.github.com>
2019-12-02 14:09:59 +0100

    Update day2pt2.py

--- 2019/tests/day2pt2.py
+++ 2019/tests/day2pt2.py
@@ -1,12 +1,17 @@
 # start pt2 8:55 - paused 9:30
-intcode = []
+# unpaused 13:30 - solved pt2 13:50
+
 for noun in range(100):
-  for verb in range(100):
-    # print( noun, verb )
-    intcode[1] = noun
-    intcode[2] = verb
-    
-    result = calcPt1(intcode) #IndexError: list assignment index out of range
-   
-    if result[0] == 19690720:
-      print( (100*noun)+verb )
+    for verb in range(100):
+        filepath = 'input_2019-2.txt'
+        with open(filepath) as fp:
+            xintcode = [int(x) for x in fp.readline().split(",")]
+            # print( noun, verb )
+            xintcode[1] = noun
+            xintcode[2] = verb
+            
+            result = calcPt1(xintcode) #IndexError: list assignment index out of range
+        
+            if result[0] == 19690720:
+                print( (100*noun)+verb )
+                break
