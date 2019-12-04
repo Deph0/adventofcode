87ade92f32f05257d85c0dc13d391e19599e830b
Deas <neo_graal@hotmail.com>
2019-12-04 19:32:39 +0100

    Solved day 4 pt 2

--- 2019/passwordSearch_pt2.py
+++ 2019/passwordSearch_pt2.py
@@ -1,5 +1,9 @@
 # AdventOfCode 2019 day 4 pt 2
 # https://adventofcode.com/2019/day/4
+# started 7:15-paused 8:00
+# started over at 9:05 - 10:00
+# pt2 unsolved 10:00-11:30, 11:45-12:00
+# start 19:00pm-19:25pm solved
 
 low, high = [171309,643603]
 
@@ -25,34 +29,16 @@ for i in range(low, high+1):
   # forloop fell though (break)- heres the values that worked out- still for-loop'ing
   # https://book.pythontips.com/en/latest/for_-_else.html
   else:
+    ddtimes = 0
     if hasdoubledigit:
-      # print(i, doubledigit, str(i).count(doubledigit))
-      # possiblepasswords += 1
-      # find how many dimes doubledigit exists inside i
-      # print(doubledigits) # [ '7', '7','9', '9', '9' ]
-      ddtimes = 0
-      prev = ''
-      for dd in doubledigits:
-          if dd == prev:
-              continue
-          if  doubledigits.count(dd) == 2:
-              possiblepasswords += 1
-          prev = dd
+        # find how many dimes doubledigit exists inside i
+        prev = ''
+        for dd in doubledigits:
+            if dd == prev: continue
+            if doubledigits.count(dd) == 2:
+                ddtimes += 1
+            prev = dd
+        if ddtimes > 0:
+            possiblepasswords += 1
 
-
-print(possiblepasswords) # pt1: 1625, pt2: ?
-# 930 too low
-# 469 too low
-
-"""
-x = ['4', '4', '6', '6', '6', '6']
-
-h = 0
-prev = ''
-for y in x:
-    if y == prev: continue
-    if  x.count(y) == 2: h+=1
-    print(x, y, x.count(y) )
-    prev = y
-print(h)
-"""
+print(possiblepasswords) # pt2: 1111
