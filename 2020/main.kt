fd3af38e489367fdc28c0325681674ead4941be0
Deph0 <Deph0@users.noreply.github.com>
2020-12-06 02:17:10 +0100

    Added main kt file
    added a readfile function, i promise ill use it in day6, and potentially refactor older to use it! I promise! :|

---
+++ 2020/main.kt
@@ -0,0 +1,12 @@
+import java.io.*
+fun input(f: String): List<String> = File(f).bufferedReader().use { it.readLines() }
+
+fun main(args: Array<String>) {
+  // day1()
+  // day1pt3()
+  // day2()
+  // day3()
+  // day4()
+  // day5()
+  day6()
+}
