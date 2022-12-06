4f6deebae10d967fae3e54504c3f5da60d8d3554
Deph0 <Deph0@users.noreply.github.com>
2022-12-06 12:08:55 +0000

    Added example for day1 2022, solved

---
+++ 2022/day1/example.js
@@ -0,0 +1,24 @@
+
+var i = '1000\n\
+2000\n\
+3000\n\
+\n\
+4000\n\
+\n\
+5000\n\
+6000\n\
+\n\
+7000\n\
+8000\n\
+9000\n\
+\n\
+10000'
+
+var elfs = []
+var newelf = 0
+for (var elf of i.split('\n')) {
+    if (elf == "") newelf += 1
+    if (!elfs[newelf]) elfs[newelf] = 0
+    elfs[newelf] += Number(elf)
+}
+console.log(JSON.stringify(elfs), Math.max(...elfs))
